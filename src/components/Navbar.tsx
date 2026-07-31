c4f41058225cb7b986f338c0b0de4887c615e763
lordgabriel98 <bajegabriel@gmail.com>
2026-08-01 00:45:00 +1000

    Added tailwindcss dependencies for styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 function Navbar(){
 
     return (
-        <nav>
-            <div>
+        <nav className="relative flex items-center justify-between px-8 py-5">
+            <div className="text-xl font-bold">
                 Gabriel Baje
             </div>
-            <ul>
-                <li>Home</li>
-                <li>Projects</li>
-                <li>About</li>
-                <li>Resume</li>
-                <li>Contact</li>
+            <ul className="absolute left-1/2 flex -translate-x-1/2 gap-8">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Resume</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
         </nav>
     )
