a2110afafa2d0ac70e28212085460fddb861f23f
lordgabriel98 <bajegabriel@gmail.com>
2026-07-31 22:28:55 +1000

    Converted project to use vite and typescript

--- js/copyright.js
+++
@@ -1,9 +0,0 @@
-const getYear=()=>{
-    const currentYear = new Date().getFullYear();
-
-    let copyright = document.getElementsByClassName("copyright")[0];
-
-    copyright.innerHTML=`© ${currentYear} Gabriel Baje`;
-}
-
-getYear();
