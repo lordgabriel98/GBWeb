7368ff154d4211dc0c845717d1fa484a2f38c2a2
lordgabriel98 <bajegabriel@gmail.com>
2026-08-01 00:22:02 +1000

    Added font and css with first component and page

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar(){
+
+    return (
+        <nav>
+            <div>
+                Gabriel Baje
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Resume</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+
+}
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+function Home(){
+
+    return (
+        <main>
+            <h1>Gabriel Baje</h1>
+            <h2>Software Developer</h2>
+
+            <p>
+                I build modern web applications, business systems, and data driven solutions.
+            </p>
+        </main>
+    )
+
+}
+
+export default Home;
