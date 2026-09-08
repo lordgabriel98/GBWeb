80dd8858b9dbb1f4e6d9fd7e59a2e49728b40df8
lordgabriel98 <bajegabriel@gmail.com>
2026-09-08 22:11:10 +1000

    Updated server file and removed logs

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,3 @@ app.use("/api/contact", contactRoute);
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
 })
-
-console.log("SMTP HOST:", process.env.SMTP_HOST);
-console.log("SMTP PORT:", process.env.SMTP_PORT);
-console.log("SMTP USER:", process.env.SMTP_USER);
